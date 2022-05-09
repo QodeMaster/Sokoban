a59aaceb93f61fe95bb1bf248e580ba80b29969a
QodeMaster <59826211+QodeMaster@users.noreply.github.com>
2022-05-09 13:10:51 +0200

    Update Main.java

--- src/skb/Main.java
+++ src/skb/Main.java
@@ -4,7 +4,7 @@ import javax.swing.JFrame;
 
 public class Main { /* Note to Self: Main ska vara game1, game2 i mobil etc. */
 	/* T.ex. main ska inte extend:a frame om spelet endast sker i  konsolen*/
-	/* ^ Eftersom Main extend:ar JFrame, ända till 2022-05-06 16:44 ^ */
+	/* ^ Eftersom Main extend:ar JFrame, Ã¤nda till 2022-05-06 16:44 ^ */
 
    public static void main(String args[]) {
 	   String[] adress = new String[] { 
@@ -16,61 +16,69 @@ public class Main { /* Note to Self: Main ska vara game1, game2 i mobil etc. */
         		"C:\\Users\\davazi20\\eclipse-workspace\\skban\\wall.png"
         };
 	   int[][] lvl1 = new int[][] {
-		   new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-		   new int[] {0, 0, 0, 5, 5, 5, 5, 5, 0, 0},
-		   new int[] {0, 5, 5, 5, 0, 0, 0, 5, 0, 0},
-		   new int[] {0, 5, 0, 0, 2, 0, 0, 5, 0, 0},
-		   new int[] {0, 5, 5, 5, 0, 0, 0, 5, 0, 0},
-		   new int[] {0, 5, 1, 5, 5, 0, 0, 5, 0, 0},
-		   new int[] {0, 5, 0, 5, 0, 1, 0, 5, 5, 0},
-		   new int[] {0, 5, 2, 0, 0, 0, 0, 0, 5, 0},
-		   new int[] {0, 5, 0, 0, 0, 0, 0, 0, 5, 0},
-		   new int[] {0, 5, 5, 5, 5, 5, 5, 5, 5, 0},
-		   new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+		   new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		   new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		   new int[] {0, 0, 0, 0, 0, 5, 5, 5, 5, 5, 0, 0, 0, 0},
+		   new int[] {0, 0, 0, 5, 5, 5, 0, 0, 0, 5, 0, 0, 0, 0},
+		   new int[] {0, 0, 0, 5, 0, 0, 2, 0, 0, 5, 0, 0, 0, 0},
+		   new int[] {0, 0, 0, 5, 5, 5, 0, 0, 0, 5, 0, 0, 0, 0},
+		   new int[] {0, 0, 0, 5, 1, 5, 5, 0, 0, 5, 0, 0, 0, 0},
+		   new int[] {0, 0, 0, 5, 0, 5, 0, 1, 0, 5, 5, 0, 0, 0},
+		   new int[] {0, 0, 0, 5, 2, 0, 0, 0, 0, 0, 5, 0, 0, 0},
+		   new int[] {0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0},
+		   new int[] {0, 0, 0, 5, 5, 5, 5, 5, 5, 5, 5, 0, 0, 0},
+		   new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		   new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		   new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 	   };
 	   int[][] lvl1MarkedSpots = new int[][] {
-		   new int[] { 6, 5 },
-		   new int[] { 5, 2 }
+		   new int[] { 7, 7 },
+		   new int[] { 6, 4 }
 		   //new int[] { 3, 2 }
 	   };
-	   int playerI = 3;
-	   int playerJ = 3;
+	   int playerI = 4;
+	   int playerJ = 4;
 	   lvl1[playerI][playerJ] = 4;
 	   Level level1 = new Level(lvl1, lvl1MarkedSpots);
 	   
 	   int[][] lvl2 = new int[][] {
-	    new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	    new int[] {0, 0, 0, 0, 0, 5, 5, 5, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-		new int[] {0, 0, 0, 0, 0, 5, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-		new int[] {0, 0, 0, 0, 0, 5, 2, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-		new int[] {0, 0, 0, 5, 5, 5, 0, 0, 2, 5, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-		new int[] {0, 0, 0, 5, 0, 0, 2, 0, 0, 2, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-		new int[] {0, 5, 5, 5, 0, 5, 0, 5, 5, 5, 0, 5, 0, 0, 0, 0, 0, 0, 5, 5, 5, 5, 5, 5, 0},
-		new int[] {0, 5, 0, 0, 0, 5, 0, 5, 5, 5, 0, 5, 5, 5, 5, 5, 5, 5, 5, 0, 0, 0, 1, 5, 0},
-		new int[] {0, 5, 0, 2, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 5, 0},
-		new int[] {0, 5, 5, 5, 5, 5, 0, 5, 5, 5, 5, 0, 5, 0, 5, 5, 5, 5, 5, 0, 0, 0, 0, 5, 0},
-		new int[] {0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 5, 5, 5, 0, 0, 5, 5, 5, 5, 5, 5, 5, 0},
-		new int[] {0, 0, 0, 0, 0, 5, 5, 5, 5, 5, 5, 5, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-		new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+		   	new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		   	new int[] {0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 0},
+			new int[] {0, 5, 0, 0, 5, 5, 0, 0, 1, 1, 5, 5, 5, 0},
+			new int[] {0, 5, 0, 2, 0, 0, 0, 0, 0, 0, 5, 5, 5, 0},
+			new int[] {0, 5, 0, 0, 0, 5, 0, 0, 0, 0, 5, 1, 5, 0},
+			new int[] {0, 5, 0, 0, 0, 5, 5, 5, 5, 5, 5, 0, 5, 0},
+			new int[] {0, 5, 0, 0, 0, 5, 5, 0, 0, 0, 0, 0, 5, 0},
+			new int[] {0, 5, 0, 0, 0, 5, 5, 0, 0, 0, 0, 0, 5, 0},
+			new int[] {0, 5, 0, 0, 0, 5, 5, 0, 0, 2, 2, 0, 5, 0},
+			new int[] {0, 5, 0, 0, 0, 5, 5, 0, 0, 0, 0, 0, 5, 0},
+			new int[] {0, 5, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 5, 0},
+			new int[] {0, 5, 0, 0, 0, 5, 5, 1, 0, 0, 0, 0, 5, 0},
+			new int[] {0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 0},
+		   	new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 	   };
 	   int[][] lvl2MarkedSpots = new int[][] {
-		   new int[] { lvl2.length - 5, lvl2[0].length - 3},
-		   new int[] { lvl2.length - 6, lvl2[0].length - 3}
-		   //new int[] { 3, 2 }
+		   new int[] { lvl2.length - 3, 7},
+		   new int[] { 4, lvl2[0].length - 3},
+		   new int[] { 2, 8 },
+		   new int[] { 2, 9 }
 	   };
-	   /*playerI = lvl2.length - 4;
-	   playerJ = 13;
 	   lvl2[playerI][playerJ] = 4;
-	   Level level2 = new Level(lvl2, lvl2MarkedSpots);*/
+	   Level level2 = new Level(lvl2, lvl2MarkedSpots);
 	   
 	   Model model = new Model(adress, playerI, playerJ, level1);
-	   View view   = new View(model);
-	   Controller controller  = new Controller(model, view); 
+	   // model.setLevel(adress, playerI, playerJ, level1);
+	   // Alt: Level[Level1, Level2];
+	   SwingView swingView   = new SwingView(model);
+	   ConsoleView consoleView = new ConsoleView(model);
+	   Controller controller   = new Controller(model);
 	   
-        JFrame frame = new JFrame("Sokoban");
-	   //Main frame = new Main(new View(adress, playerI, playerJ, level2));
+	   controller.addView(swingView);
+	   controller.addView(consoleView);
+	   
+	   JFrame frame = new JFrame("Sokoban");
         
-        frame.setContentPane(view);
+        frame.setContentPane(swingView);
         frame.addKeyListener(controller);
         
         frame.setResizable(false);
