a87b99050903a3e6d07eea6df8f7eb89073e5648
QodeMaster <59826211+QodeMaster@users.noreply.github.com>
2022-05-10 10:12:19 +0200

    Removed "keyboard implementation"

--- src/skb/Controller.java
+++ src/skb/Controller.java
@@ -4,7 +4,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
 
-public class Controller implements KeyListener {
+public class Controller {
 	Model model;
 	ArrayList<Update> view;
 	Level lvl;
@@ -13,41 +13,10 @@ public class Controller implements KeyListener {
 		this.model = model;
 		this.view = new ArrayList<>();
 	}
-	
-	
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
 
-	@Override
-	public void keyPressed(KeyEvent e) {
-	    int keyCode = e.getKeyCode();
-	    switch( keyCode ) { 
-	        case KeyEvent.VK_UP:
-	        	model.move(-1, 0, view);
-	            break;
-	        case KeyEvent.VK_DOWN:
-	        	model.move(1, 0, view);
-	            break;
-	        case KeyEvent.VK_LEFT:
-	        	model.move(0, -1, view);
-	            break;
-	        case KeyEvent.VK_RIGHT:
-	        	model.move(0, 1, view);
-	            break;
-	        default:
-	        	System.out.println("Invalid");
-	     }
+	void move(int di, int dj) {
+		model.move(di, dj, view);
 	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
 
 	public void addView(Update view) {
 		this.view.add(view);
