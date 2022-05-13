4dcf0fee6c3f0f7e2211df27c76ce6e8720b5e36
QodeMaster <59826211+QodeMaster@users.noreply.github.com>
2022-05-13 12:47:43 +0200

    Made private accessable

--- src/skb/Level.java
+++ src/skb/Level.java
@@ -1,12 +1,12 @@
 package skb;
 
 public class Level {
-	int[][] markedPoints;
-	int[][] level;
-	int coveredPoints;
-	int totPoints;
-	int i;
-	int j;
+	private int[][] markedPoints;
+	private int[][] level;
+	private int coveredPoints;
+	private int totPoints;
+	private int i;
+	private int j;
 	
 	public Level(int[][] level, int[][] map, int startPosI, int startPosJ) {
 		this.level		  = level;
@@ -24,5 +24,18 @@ public class Level {
 			this.markedPoints[coord[0]][coord[1]] = 1;
 		}
 	}
+	
+	int getLevelLength() { return level.length; }
+	int getLevelRowLength() { return level[0].length; }
+	int getLevelCoordValue(int I, int J) { return level[I][J]; }
+	int getMarkerCoordValue(int I, int J) { return markedPoints[I][J]; }
+	int getTotPoints() { return totPoints; }
+	int getCoveredPoints() { return coveredPoints; }
+	int getStartPosI() { return i; }
+	int getStartPosJ() { return j; }
+	
+	void updateLevelMapCoord(int I, int J, int value) { level[I][J] = value; }
+	void updateMarkerMapCoord(int I, int J, int value) { markedPoints[I][J] = value; }
+	void updateCoveredPoints(int value) { coveredPoints = value; }
 
 }
